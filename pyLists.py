c61911c3d0c8ff0fb75689fc7d4a97f1cfb06957
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:27:54 -0300

    Added file pyLists
    Exercise to sort or choose elements in a list

---
+++ pyLists.py
@@ -0,0 +1,10 @@
+# lists can store multiple data types ex:
+# friends = ["Kevin", 2, True]
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Tobi"]
+friends[1]="Lia"
+print(friends)
+print(friends[1])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:3])
