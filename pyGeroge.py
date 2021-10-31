1b2891aa0127c9048f622d84e590c357faec73ac
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:21:07 -0300

    Added file pyGeroge
    Short storie to demosntrate use of variables

---
+++ pyGeroge.py
@@ -0,0 +1,7 @@
+character_name = "Tom"
+character_age = "50"
+print("There onde was a man name " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but he didn´t like being " + character_age +".")
