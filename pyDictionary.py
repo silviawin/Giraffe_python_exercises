47678c9e966787f6141925bf69d131d660667a3c
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:16:57 -0300

    Added file pyDictionary
    Simple exercise demonstrating the use of a dictionary

---
+++ pyDictionary.py
@@ -0,0 +1,19 @@
+month_conversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(month_conversions["Nov"])
+# other way
+print(month_conversions.get("Dec"))
+print(month_conversions.get("Luv", "Not a valid key"))
