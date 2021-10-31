b14f036dde42b0a1d3dc6ad9b855730992bfddb0
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:50:47 -0300

    Added file pyWhile
    Basic use of while loop

---
+++ pyWhile.py
@@ -0,0 +1,11 @@
+# while loop is for executing the same block of code multiple times
+# when you create a while loop the first thing is to create an integer
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1  # i = i + 1
+
+print("Done with loop")
+
