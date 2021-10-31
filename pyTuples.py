ea23888002c933b353d14aeec0b72b32cabbb1c2
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:53:21 -0300

    Added file pyTuples
    Demonstration of use of tupple

---
+++ pyTuples.py
@@ -0,0 +1,9 @@
+# tuples () are different from lists[] because:
+# 1) tuples are immutable;
+# 2) you can create a list of tuples;
+
+coordinates = (4, 5)
+print(coordinates)
+print(coordinates[0])
+
+coordinates_many = [(4,5), (6,7), (8,9)]
