edc74b8e6f7063fe2d1e0d56bdd5e51b5bc6722b
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:35:59 -0300

    Added file pyNumbers
    Demonstration of use of math module

---
+++ pyNumbers.py
@@ -0,0 +1,27 @@
+from math import *
+print(2)
+print(2.98887)
+print(-4.8787)
+print(3 + 4)
+print(3 + 4.5)
+print(4 - 3)
+print( 12 / 4)
+print(12 * 3)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 5
+print(my_num)
+print(str(my_num))
+print(str(my_num) + " is my fav number.")
+second_number = -5
+print(abs(second_number))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.2))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
+
