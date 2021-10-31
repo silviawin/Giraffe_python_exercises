4f3c99fc7162cd101b8ad03bbce75426add9d6b9
Silvia Winetzki <silviawinetzki@gmail.com>
2021-10-31 13:48:48 -0300

    Added file pyStrings
    Use of in-built python functions for strings

---
+++ pyStrings.py
@@ -0,0 +1,16 @@
+print("Giraffe \n Academy")
+print("Giraffe \"Academy\"")
+print("Giraffe\Academy")
+
+phrase = "Giraffe 2"
+print(phrase + " is cool")
+
+phrase_2 = " Is cool"
+print(phrase_2.lower())
+print(phrase_2.upper())
+print(phrase_2.isupper())
+print(phrase_2.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("G"))
+print(phrase.replace("Giraffe", "Elephant"))
